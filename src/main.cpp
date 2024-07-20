86187f621ce503fd076beb44bfdc18366c1d97a5
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2024-07-20 19:00:35 +0200

    small refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "halvoeAtSPIHost.hpp"
 
 halvoeDVI::AtHost::SPILink dviSPILink;
+elapsedMicros timeSinceLastFrame;
+unsigned long frameInterval = 30000; // micro seconds
 GFXcanvas8 frame(320, 240);
 uint16_t x = 0;
 uint16_t y = 5;
@@ -15,11 +17,12 @@ void setup()
 
   dviSPILink.begin();
   Serial.println("Host leaves setup.");
+  timeSinceLastFrame = 0;
 }
 
 void loop()
 {
-  if (dviSPILink.isDVIReady())
+  if (dviSPILink.isDVIReady() && timeSinceLastFrame >= frameInterval)
   {
     frame.fillScreen(0);
     frame.fillRect(x, y, 25, 25, 255);
@@ -27,6 +30,6 @@ void loop()
 
     Serial.println("dviSPILink.transferFrame(frame)");
     dviSPILink.transferFrame(frame);
-    delay(40);
+    timeSinceLastFrame = 0;
   }
 }
